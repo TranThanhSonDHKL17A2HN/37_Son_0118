8159d9a16a7bc14cbab39c54d4aa0b19711c6b65
Khoa hoc du lieu 17A2 <ttson.dhkl17a2hn@sv.uneti.edu.vn>
2024-01-08 23:00:30 +0700

    Them bai tap chuong 9

---
+++ 9.6.py
@@ -0,0 +1,10 @@
+def kiem_tra_so_nguyen_to(n):
+    so_uoc=0
+    for i in range(1,n+1):
+        if n%i==0:
+            so_uoc+=1
+    if so_uoc==2:
+        return True
+    return False
+n=int(input("Nhập số cần kiểm tra: "))
+print(kiem_tra_so_nguyen_to(n))
---
+++ 9.7.py
@@ -0,0 +1,7 @@
+def chia_lay_nguyen(a,b):
+    c=a//b
+    return c 
+a=int(input("a = "))
+b=int(input("b = "))
+c= chia_lay_nguyen(a,b)
+print("Phần nguyên của",a,"chia",b,"là :",c) 
